da7e68e5c4d34964081a7f56e93dcc9d3048ab75
ValidMo <mohammadivalid@gmail.com>
2024-03-29 23:31:06 +0330

    added MessageView

---
+++ Sentence AI/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Sentence AI
+//
+//  Created by Valid Mohammadi on 29.03.2024.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
